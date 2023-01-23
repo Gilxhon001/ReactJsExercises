857895b4f72c23e1ee02bcbc6bfc961dc46aa6c2
Gilxhon <gilxhonhima@gmail.com>
2023-01-23 18:05:28 +0100

    Props - 04 Finished

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name="Chelli" age={22} />
+                <Welcome name={<strong>Chelli</strong>} age={22} />
             </div>
         )
     }
