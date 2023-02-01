bdbfa18615c7dcd1bf3889255c49feafa9c45aa8
Gilxhon <gilxhonhima@gmail.com>
2023-02-01 20:07:35 +0100

    Events - 04

--- src/ClickTracker.js
+++ src/ClickTracker.js
@@ -16,9 +16,9 @@ export default class ClickTracker extends React.Component {
             {!this.state.name && <h1>Click a button</h1>}
             {this.state.name && <h1>{this.state.name} was CLICKED !</h1>}
 
-            <button name="Button 1" onClick={this.eventHandler}>Button 1</button>
-            <button name="Button 2" onClick={this.eventHandler}>Button 2</button>
-            <button name="Button 3" onClick={this.eventHandler}>Button 3</button>
+            <button name="Button 1" onClick={this.eventHandler}><img src="/logo192.png" alt="img1"/></button>
+            <button name="Button 2" onClick={this.eventHandler}><img src="/logo192.png" alt="img2"/></button>
+            <button name="Button 3" onClick={this.eventHandler}><img src="/logo192.png" alt="img3"/></button>
             </>
         )
     }
