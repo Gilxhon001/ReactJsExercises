cdcaeb5d94db627e55567097c84ecadf314db58a
Gilxhon <gilxhonhima@gmail.com>
2023-02-07 01:15:14 +0100

    useState---02 Finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const root = document.querySelector('#root')
+const root = ReactDOM.createRoot(
+    document.querySelector('#root')
+  );
 
-ReactDOM.render(<App/>, root)
+root.render(<App/>)
