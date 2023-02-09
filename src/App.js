ac86fa50b379206179bfaf34163f11c19a30343a
Gilxhon <gilxhonhima@gmail.com>
2023-02-09 11:35:35 +0100

    useEffect - 03 Finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-// import TodoList from "./TodoList";
 import Container from "./Container";
-import { ClickCounter } from "./ClickCounter";
 import Counter from "./Counter";
 import GithubUser from "./GithubUser";
 
 const App = () => {
-
-    const onCounterChange = (count) => {
-        console.log(`The count is now ${count}`);
-    };
 
     return (
         <Container tittle="To Do List">
