4ee90c316352417299c9feb9b9e4ec079e4f5eab
Gilxhon <gilxhonhima@gmail.com>
2023-02-09 11:08:13 +0100

    useEffect - 01 Finished

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -21,33 +21,3 @@ export const ClickCounter = ({incrementValue=1, startingValue=0, onCounterChange
         </div>
     )
 }
-
-
-// export default class ClickCounter extends React.Component {
-
-//     state = {
-//         count: this.props.initialValue,
-//     }
-
-//     handleCounterIncrement = () => {
-//         this.setState((state) =>{
-//             return {count: state.count + this.props.incrementValue}
-//         })
-//     }
-
-
-//     render() {
-//         return (
-//             <div>
-//                 <h3>Count: {this.state.count}</h3>
-//                 <CounterButton handleInc={this.handleCounterIncrement} />
-//                 {/* We pass the handle increment function with props */}
-//             </div>
-//         )
-//     }
-// }
-
-// ClickCounter.defaultProps = {
-//     initialValue: 1,
-//     incrementValue: 1
-// }
