eabaf811fe63caac002c109f80e6adc4e5afe501
Gilxhon <gilxhonhima@gmail.com>
2023-02-12 21:10:02 +0100

    FilteredList Finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { ClickCounter } from "./ClickCounter";
+import FilteredList from "./FilteredList";
 import Container from "./Container";
 
 const App = () => {
+    const list = [
+        { name: "John", id: 1, age: 20 },
+        { name: "Jane", id: 2, age: 15 },
+        { name: "Jim", id: 3, age: 25 },
+        { name: "Joe", id: 4, age: 30 },
+        { name: "Jota", id: 2, age: 18 },
+    ];
 
     return (
-        <Container tittle="Counter">
-            <ClickCounter initialValue={0} />
+        <Container tittle="Filtered List">
+            <FilteredList list={list} />
         </Container>
     )
 }
 
-export default App ;
+export default App;
